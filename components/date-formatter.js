a5fcb629a78db291e132c6d87d5a8c614fb8f968
angelyoung24 <angel.m.young24@gmail.com>
2021-01-16 22:09:00 -0500

    first commit

--- components/date-formatter.js
+++ components/date-formatter.js
@@ -2,5 +2,9 @@ import { parseISO, format } from 'date-fns'
 
 export default function DateFormatter({ dateString }) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  return (
+  <div>
+  <time className="justify-center" dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  </div>
+  )
 }
