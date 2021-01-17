da02f9c262b2635f306322b870b1029a08402c82
Angel Young <angelyoung@Angels-iMac.local>
2021-01-16 23:30:17 -0500

    added head to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,13 @@
 import Layout from "../components/layout"
+import Head from "../components/head"
 import Container from "../components/container"
 
 export default function Contact() {
     return (
         <Layout>
+            <Head>
+					<title>Weekly Bites with Angel</title>
+				</Head>
             <Container>
                 <h1>Contact</h1>
             </Container>
